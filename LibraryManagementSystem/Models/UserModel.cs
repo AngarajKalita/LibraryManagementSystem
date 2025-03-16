fb78ec00e4c2f5df42d59e7c330c0144167f485d
AngarajKalita <angarajkalita999@gmail.com>
2025-03-16 23:35:54 +0530

    Created UserModel, Implemented Validation

---
+++ LibraryManagementSystem/Models/UserModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace LibraryManagementSystem.Models
+{
+    public class UserModel
+    {
+
+        [Key]
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; } = string.Empty;
+    }
+}

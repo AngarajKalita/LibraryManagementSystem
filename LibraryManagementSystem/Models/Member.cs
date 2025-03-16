f460db659350eb44b182fe67ae575ef8593a9b20
AngarajKalita <angarajkalita999@gmail.com>
2025-03-16 23:42:40 +0530

    Created Member Model, implemented validation

--- LibraryManagementSystem/Models/Member.cs
+++ LibraryManagementSystem/Models/Member.cs
@@ -1,9 +1,17 @@
-﻿namespace LibraryManagementSystem.Models
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+namespace LibraryManagementSystem.Models
 {
     public class Member
     {
+        [Key]
         public int MemberId { get; set; }
+        [Required(ErrorMessage = "Name is required")]
+        [StringLength(50, ErrorMessage = "Name cannot be longer than 50 characters")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
         public string Email { get; set; }
         public DateTime MembershipStartDate { get; set; }
     }

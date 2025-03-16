fb32953f14bd6ef6023669ffb21394617dacfd80
AngarajKalita <angarajkalita999@gmail.com>
2025-03-16 23:57:27 +0530

    UserModels Controller created

---
+++ LibraryManagementSystem/Controllers/UserModelsController.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using LibraryManagementSystem.Data;
+using LibraryManagementSystem.Models;
+
+namespace LibraryManagementSystem.Controllers
+{
+    public class UserModelsController : Controller
+    {
+        private readonly LibraryContext _context;
+
+        public UserModelsController(LibraryContext context)
+        {
+            _context = context;
+        }
+
+        // GET: UserModels
+        public async Task<IActionResult> Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Login(UserModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                // Here you would typically check the user's credentials against a database
+                // For demonstration purposes, we'll assume the login is successful if the email is "admin@example.com" and the password is "password"
+
+                var userModel = _context.Members.FirstOrDefault(m => m.Email == model.Email);
+                //Console.WriteLine(userModel);
+                //var adminModel = _context.AdminModel.FirstOrDefault(m => m.Email == model.Email && m.Password == model.Password);
+                
+                if (userModel != null)
+                {
+                    // Redirect to the home page or another action
+                    return RedirectToAction("Borrow", "Books");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                }
+            }
+
+            return View(model);
+        }
+
+        // GET: UserModels/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var userModel = await _context.UserModel
+                .FirstOrDefaultAsync(m => m.Email == id);
+            if (userModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(userModel);
+        }
+
+        // GET: UserModels/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UserModels/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Email,Password")] UserModel userModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(userModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(userModel);
+        }
+
+        // GET: UserModels/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var userModel = await _context.UserModel.FindAsync(id);
+            if (userModel == null)
+            {
+                return NotFound();
+            }
+            return View(userModel);
+        }
+
+        // POST: UserModels/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("Email,Password")] UserModel userModel)
+        {
+            if (id != userModel.Email)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(userModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserModelExists(userModel.Email))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(userModel);
+        }
+
+        // GET: UserModels/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var userModel = await _context.UserModel
+                .FirstOrDefaultAsync(m => m.Email == id);
+            if (userModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(userModel);
+        }
+
+        // POST: UserModels/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(string id)
+        {
+            var userModel = await _context.UserModel.FindAsync(id);
+            if (userModel != null)
+            {
+                _context.UserModel.Remove(userModel);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UserModelExists(string id)
+        {
+            return _context.UserModel.Any(e => e.Email == id);
+        }
+    }
+}
